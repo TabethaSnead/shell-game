5c47fd8ce3c4118d3489d1a32bdd5dc6a1ebab57
TabethaSnead <tabetha.snead@gmail.com>
2023-01-10 12:20:45 -0800

    centered imgs, buttons, and result sections in css

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 /* Imports */
 
 /* Get DOM Elements */
+const hobbitImg = document.getElementById('hobbit-img');
+const mountianImg = document.getElementById('mountian-img');
+const treesImg = document.getElementById('trees-img');
+
+const hobbitButton = document.getElementById('hobbit-button');
+const mountianButton = document.getElementById('mountian-button');
+const treesButton = document.getElementById('trees-button');
+
+const correctSpan = document.getElementById('correct-span');
+const incorrectSpan = document.getElementById('incorrect-span');
+const totalSpan = document.getElementById('total-span');
 
 /* State */
-
+let correctGuesses = 0;
+let totalGuesses = 0;
 /* Events */
+hobbitButton.addEventListener('click', () => {
+    console.log('hobbit');
+});
+
+mountianButton.addEventListener('click', () => {
+    console.log('mountian');
+});
+
+treesButton.addEventListener('click', () => {
+    console.log('trees');
+});
 
 /* Display Functions */
 
